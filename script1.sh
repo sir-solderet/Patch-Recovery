0a9a864760c5aba3c155ac6793376eedde681311
Solderet <31514860+sir-solderet@users.noreply.github.com>
2025-08-05 06:03:35 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if file "$1" | grep -q "LZ4 compressed data"; then
-    cp "$1" r.img.lz4
-	lz4 -B6 --content-size -f r.img.lz4 r.img
-else
-    cp "$1" r.img
+if [ -f recovery.img.lz4 ];then
+	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
 fi
+
+#off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
+#dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
+cp recovery.img r.img
 
 if [ ! -f phh.pem ];then
     openssl genrsa -f4 -out phh.pem 4096
